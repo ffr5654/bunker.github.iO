3970397c833bb2201d6c104fbe91ebc42bdf2816
ffr5654 <141408109+ffr5654@users.noreply.github.com>
2024-09-02 23:09:39 +0530

    Add files via upload

---
+++ bunkervpn.website/script.js
@@ -0,0 +1,9 @@
+// Example: Smooth Scrolling for Navigation Links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
